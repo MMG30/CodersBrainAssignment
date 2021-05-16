541dadf0c6bfbf7eb8b057e342d0f6239f75854c
MMG30 <53515423+MMG30@users.noreply.github.com>
2021-05-17 00:08:43 +0530

    Delete SpaceX Assignment directory

--- SpaceX Assignment/src/Utils/API_URL.js
+++
@@ -1,4 +0,0 @@
-export const RenderPage_URL = 'https://api.spacexdata.com/v3/launches?limit=100';
-export const LaunchSuccess_URL = "https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true";
-export const LaunchANDLandSuccess_URL = "https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true&amp;land_success=true"
-export const ALL_URL = "https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true&amp;land_success=true&amp;launch_year=2014"
